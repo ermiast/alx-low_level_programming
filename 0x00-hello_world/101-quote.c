7ca8ab9005cd56b04157eb835c410b04d5e30fb5
ermiast <ligachayo@gmail.com>
2021-02-04 20:49:48 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- * Return:1
+ * main - Changes stderr without using printf or puts
+ *
+ * Return: Always 1
  */
+
 int main(void)
 {
-  char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, ch, sizeof(ch));
+  write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
   return (1);
 }
