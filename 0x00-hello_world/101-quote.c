6e6e2a3840bb8a0d378f80ef15c3a1564d3edc1f
ermiast <ligachayo@gmail.com>
2021-02-05 17:08:08 +0000

    warnining

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
-* main - Entry point
-* Returrn:1
-*/
+/**
+ * main - Entry point
+ * Returrn:1
+ */
 
 int main(void)
-/*
-* for main
-*/
 
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
