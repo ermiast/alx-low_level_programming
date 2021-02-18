051c26b813f8687c6001a3d89a76ac8e968bb99f
ermiast <ligachayo@gmail.com>
2021-02-18 07:35:30 +0000

    spaces aroind '-' removed, kernel style code

---
+++ 0x05-pointers_arrays_strings/101-crackme.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <termios.h>
+#include <unistd.h>
+
+
+
+len = Username_length
+For i = len-1 to 0 and j = 0 len-1
+NewPass[j] = Username[i] + i
