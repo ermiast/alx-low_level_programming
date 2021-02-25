f4997847290814ce87143f2eb2e3ce957ef8d4ae
ermiast <ligachayo@gmail.com>
2021-02-25 21:06:14 +0000

    palendrome

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -10,3 +10,6 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int check_prime(int n, int resp);
 int square(int n, int val);
+int last_index(char *s);
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int pair);
